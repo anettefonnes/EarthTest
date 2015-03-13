c057456d553cd4480cfee1231b2627d491d7e808
Christer Steinfinsboe <csteinfinsbo@gmail.com>
2015-03-13 15:22:14 +0100

    Changed name

--- World.js
+++ World.js
@@ -1,31 +1,38 @@
 function World(){
+    this.root = new THREE.Object3D();
 
     this.rotation += 0.005;
+    //this.rad = 100;
+    //this.seg = 300;
 
     // Theoretical Constants
     //The root is the referance point for world surface and orbiting objects
-    this.root = new THREE.Object3D;
+
 
     //the sphere is the surface of the planet, after is the following constants to the texture
-    this.seg = 300;
+    /*
     this.texture = 'img/earth3.jpg';
     this.bumpMap = 'img/earth3-bump.jpg';
     this.bumpScale = 2;
     this.specularMap = 'img/earth3_water.png';
     this.specular = 'grey';
-    this.root.add(this.sphere);
-
-    this.sphere =  THREE.Mesh(
-        new THREE.SphereGeometry(this.rad, this.seg, this.seg),
+    this.root.add(new THREE.Mesh(
+                    new THREE.SphereGeometry(this.rad, this.seg),
+                    new THREE.MeshPhongMaterial({
+                        map:         THREE.ImageUtils.loadTexture(this.texture),
+                        bumpMap:     THREE.ImageUtils.loadTexture(this.bumpMap),
+                        bumpScale:   this.bumpScale,
+                        specularMap: THREE.ImageUtils.loadTexture(this.specularMap),
+                        specular:    new THREE.Color(this.specular)
+                      })));
+    */
+    this.root.add(new THREE.Mesh(
+        new THREE.SphereGeometry(100, 300),
         new THREE.MeshPhongMaterial({
-            map:         THREE.ImageUtils.loadTexture(this.texture),
-            bumpMap:     THREE.ImageUtils.loadTexture(this.bumpMap),
-            bumpScale:   this.bumpScale,
-            specularMap: THREE.ImageUtils.loadTexture(this.specular),
-            specular:    new THREE.Color()
-        }));
-
-    this.rotate = function(){
-        this.root.rotation.y += this.rotation;
-    }
+            map:         THREE.ImageUtils.loadTexture( 'img/earth3.jpg'),
+            bumpMap:     THREE.ImageUtils.loadTexture('img/earth3-bump.jpg'),
+            bumpScale:   2,
+            specularMap: THREE.ImageUtils.loadTexture('img/earth3_water.png'),
+            specular:    new THREE.Color( 'grey')
+        })));
 }
--- earth.js
+++ earth.js
@@ -1,5 +1,5 @@
 
-var scene, camera, renderer, camPosZ, camposX, rotation;
+var scene, camera, renderer, rotation;
 var world, material, light, stars, worldEvent;
 
 
@@ -17,9 +17,8 @@ function init() {
     scene.add(world.root);
 
     camera = new THREE.PerspectiveCamera(45, width / height, 1, 10000);
-    camPosZ = 600;
-    camposX = 0;
-    camera.position.z = camPosZ;
+
+    camera.position.z = 600;
 
     renderer = new THREE.WebGLRenderer();
     renderer.setSize(width, height);
@@ -31,10 +30,9 @@ function init() {
     var gem = new THREE.SphereGeometry(3, 6, 6);
     var mat = new THREE.MeshBasicMaterial({color: 0xffff00});
     worldEvent = new THREE.Mesh(gem, mat);
-    worldEvent.rotateByAxis(sphere.y, 30);
     worldEvent.position.z = 100;
     scene.add(worldEvent);
-    sphere.add(worldEvent);
+    world.root.add(worldEvent);
 
 
     //light adding
@@ -43,7 +41,7 @@ function init() {
     light.position.set(200,0,500);
     scene.add(light);
 
-    stars = createStars(rad, seg);
+    stars = createStars(100, 200);
     scene.add(stars);
 
     //sphere.add(camera);
@@ -69,42 +67,11 @@ function createStars(rad, seg) {
     );
 }
 
-//When an event occurres, this function runs
-function keyPressed(event) {
-    var keycode = event.which;
-    //Cursor up, down, left, right
-    if(keycode == 38) {
-        camPosZ -= 5;
-    }
-    if(keycode == 40) {
-        camPosZ += 5;
-    }
-    if(keycode == 37) {
-        camposX -= 5;
-    }
-    if(keycode == 39) {
-        camposX += 5;
-    }
-    camera.position.z = camPosZ;
-
-    //Does not work
-
-    //TODO: Get this value to automatically update the value in animate?
-    if(keycode == 107){
-        rotation += 0.01;
-    }
-    if(keycode == 109) {
-        rotation -= 0.01;
-
-    }
-}
 
 //TODO: More action in this function; rotation, light etc.?
 function animate() {
     requestAnimationFrame(animate);
 
-    world.rotate();
-    document.addEventListener("keydown", keyPressed, true);
     renderer.render(scene, camera);
 }
 
